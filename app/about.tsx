3a710b2a8c368bb61b63e267baccdfc57ec998f4
hasnain <hasnain@kali.hasnain>
2025-05-30 15:15:44 +0500

    u

--- app/about.tsx
+++ app/about.tsx
@@ -1,7 +1,5 @@
 import { View, Text } from "react-native";
 import { Link } from "expo-router";
-import BtcCard from "@/app-example/components/BtcCard";
-import BinanceTicker from "@/app-example/components/BinanceTicker";
 
 export default function AboutScreen() {
   return (
@@ -11,8 +9,6 @@ export default function AboutScreen() {
       <Link href="/">
         <Text className="text-blue-500 mt-4">Back to Home</Text>
       </Link>
-      <BtcCard/>
-      <BinanceTicker/>
     </View>
   );
 }
