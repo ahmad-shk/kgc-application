700581ec577e03647eee48b00f256165a7f6bb46
ahmad-shk <ahmadsaleem19950@gmail.com>
2025-06-03 04:10:48 +0500

    update

--- components/tradingViewChart.tsx
+++ components/tradingViewChart.tsx
@@ -1,34 +1,40 @@
+// components/TradingViewChart.tsx
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import { WebView } from 'react-native-webview';
 
 const TradingViewChart = () => {
-  const tradingViewHTML = `
+  const htmlContent = `
     <!DOCTYPE html>
     <html>
       <head>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        <title>TradingView Chart</title>
+        <style>
+          html, body, #container {
+            margin: 0;
+            padding: 0;
+            height: 100%;
+            background-color: #181A20;
+          }
+        </style>
       </head>
-      <body style="margin:0;padding:0;">
-        <div id="tradingview_widget"></div>
-
-        <script type="text/javascript" src="https://s3.tradingview.com/tv.js"></script>
-        <script type="text/javascript">
-          new TradingView.widget({
-            "autosize": true,
-            "symbol": "BINANCE:BTCUSDT",
-            "interval": "15",
-            "timezone": "Etc/UTC",
-            "theme": "dark",
-            "style": "1",
-            "locale": "en",
-            "toolbar_bg": "#161616",
-            "enable_publishing": false,
-            "withdateranges": true,
-            "hide_side_toolbar": false,
-            "allow_symbol_change": true,
-            "container_id": "tradingview_widget"
-          });
+      <body>
+        <div id="container"></div>
+        <script type="text/javascript" src="https://s3.tradingview.com/external-embedding/embed-widget-advanced-chart.js">
+        {
+          "autosize": true,
+          "symbol": "BINANCE:BTCUSDT",
+          "interval": "15",
+          "timezone": "Etc/UTC",
+          "theme": "dark",
+          "style": "1",
+          "locale": "en",
+          "backgroundColor": "#181A20",
+          "hide_side_toolbar": false,
+          "allow_symbol_change": false,
+          "support_host": "https://www.tradingview.com"
+        }
         </script>
       </body>
     </html>
@@ -38,10 +44,8 @@ const TradingViewChart = () => {
     <View style={{ flex: 1 }}>
       <WebView
         originWhitelist={['*']}
-        source={{ html: tradingViewHTML }}
-        javaScriptEnabled
-        domStorageEnabled
-        style={StyleSheet.absoluteFill}
+        source={{ html: htmlContent }}
+        style={{ flex: 1 }}
       />
     </View>
   );
